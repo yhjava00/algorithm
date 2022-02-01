7e0acc6a2637d5f709880aefbe5bff3f7e3f9e1a
yhjava00 <yhjava00@gmail.com>
2022-02-01 13:30:24 +0900

    백준 10171 고양이 https://www.acmicpc.net/problem/10171

---
+++ src/ioAndFourRulesPros/Cat.java
@@ -0,0 +1,14 @@
+package ioAndFourRulesPros;
+
+public class Cat {
+
+	public static void main( String[] args ) {
+
+		System.out.println( "\\    /\\" );
+		System.out.println( " )  ( ')" );
+		System.out.println( "(  /  )" );
+		System.out.println( " \\(__)|" );
+
+	}
+
+}
