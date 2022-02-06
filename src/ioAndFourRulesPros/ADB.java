d6dfc113e0bc473e2821e12ecec8f6dcd1d832ac
yhjava00 <yhjava00@gmail.com>
2022-02-06 17:28:28 +0900

    백준 1008 A/B https://www.acmicpc.net/problem/1008

---
+++ src/ioAndFourRulesPros/ADB.java
@@ -0,0 +1,22 @@
+package ioAndFourRulesPros;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ADB {
+
+	public static void main( String[] args ) throws IOException {
+
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		StringTokenizer st = new StringTokenizer( br.readLine(), " " );
+
+		double a = Double.parseDouble( st.nextToken() );
+		double b = Double.parseDouble( st.nextToken() );
+
+		System.out.println( a / b );
+
+	}
+
+}
