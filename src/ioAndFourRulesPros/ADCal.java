b2d69ab9cc788cd032b650185c4a7e8f29b80500
yhjava00 <yhjava00@gmail.com>
2022-02-09 20:29:13 +0900

    백준 18108 1998년생인 내가 태국에서는 2541년생?! https://www.acmicpc.net/problem/18108

---
+++ src/ioAndFourRulesPros/ADCal.java
@@ -0,0 +1,17 @@
+package ioAndFourRulesPros;
+
+import java.util.Scanner;
+
+public class ADCal {
+
+	public static void main( String[] args ) {
+
+		Scanner sc = new Scanner( System.in );
+
+		int y = sc.nextInt();
+
+		System.out.println( y - 543 );
+
+	}
+
+}
